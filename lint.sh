c0a9075f79eeae97b3ab4b7093223f1c09d40def
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-08 14:00:21 +0000

    Deploy campbe13/javascript-320 to github.com/campbe13/javascript-320.git:gh-pages

--- lint.sh
+++ lint.sh
@@ -9,9 +9,12 @@
 # using builtin version but need to install ???
 #npm install htmllint -g
 npm install -g htmllint-cli
+npm install -g eslint
 
 htmllint init
 # cannont get around class and spacing config issue
 #htmllint `find ./ -name "*.html"|grep -v node_modules`
 
 htmllint 01-html-css-review/semantics.html
+
+eslint `find ./ -name "*.js"|grep -v node_modules`
